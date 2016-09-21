0fe9ce878e820a2625270b5705d81b6f788bc785
Mario <Mario>
2016-09-21 17:59:37 -0400

    fixed code a little bit, code still breaking, saying moment is not defined

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -24,51 +24,52 @@ $("#addTrain").on("click", function() {
   // Getting the value from user input
   var trainName = $('#traininput').val().trim();
   var destination = $('#destinationinput').val().trim();
-  var firstTrain = $('#firsttraintimeinput').val().trim();
+  var firstTrain = moment($('#firsttraintimeinput').val().trim(), "HH:mm").subtract(1,"years").format("HH:mm");
   var frequency = $('#frequencyinput').val().trim();
 
-  // First Train (pushed back 1 year to make sure it comes before current time)
-  var firstTrainConverted = moment(firstTrain, "hh:mm").subtract(1, "years");
-  // Current Time
-  var currentTime = moment();
-  console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-  // Difference between the times
-  var diffTime = moment().diff(moment(firstTrainConverted), "minutes");
-  console.log("DIFFERENCE IN TIME: " + diffTime);
-  // Time apart (remainder)
-  var tRemainder = diffTime % frequency;
-  console.log(tRemainder);
-  // Minute Until Train
-  var minutesAway = frequency - tRemainder;
-  console.log("MINUTES TILL TRAIN: " + minutesAway);
-  // Next Train
-  var nextTrain = moment().add(minutesAway, "minutes");
-  console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"))
-
-  //Code for push
-  dataRef.ref().push({
-    trainName: trainName,
+  // Creates local "temporary" object for holding train data
+  var newTrain = {
+    name:  trainName,
     destination: destination,
     firstTrain: firstTrain,
-    frequency: frequency,
-    nextTrain: nextTrain,
-    minutesAway: minutesAway
-  });
+    frequency: frequency
+  }
+
+  // Uploads train data to the database
+  trainData.ref().push(newTrain);
+
   // Don't refresh the page!
   return false;
 
 });
 
-//Firebase watcher + initial loader
-dataRef.ref().on("child_added", function(childSnapshot) {
+// Function that adds trains to database
+dataRef.ref().on("child_added", function(childSnapshot, prevChildKey) {
 
-  $("#trainTable > tbody").append("<tr><td>" + childSnapshot.val().trainName + "</td><td>" + childSnapshot.val().destination + "</td><td>" + childSnapshot.val().frequency + "</td><td>" + childSnapshot.val().nextTrain + "</td><td>" + childSnapshot.val().minutesAway + "</td></tr>");
+  //Create variables to store information
+  var tName = childSnapshot.val().name;
+  var tDestination = childSnapshot.val().destination;
+  var tFrequency = childSnapshot.val().frequency;
+  var tFirstTrain = childSnapshot.val().firstTrain;
+
+  // Calculating minutes until arrival
+
+  // Difference between the times
+  var diffTime = moment().diff(moment(tFirstTrain), "minutes");
+   // Time apart (remainder)
+  var tRemainder = diffTime % tFrequency;
+  // Minute Until Train
+  var minutesAway = tfrequency - tRemainder;
+  // Next Train
+  var nextTrain = moment().add(minutesAway, "minutes").format("HH:mm");
+
+  console.log("DIFFERENCE IN TIME: " + diffTime);
+  console.log(tRemainder);
+  console.log("MINUTES TILL TRAIN: " + minutesAway);
+  console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"))
+
+  $("#trainTable > tbody").append("<tr><td>" + tName + "</td><td>" + tdestination + "</td><td>" + tfrequency + "</td><td>" + nextTrain + "</td><td>" + minutesAway + "</td></tr>");
 
 }, function(errorObject) {
     console.log("Errors handled: " + errorObject.code)
 });
-
-dataRef.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot){
-  // Change the HTML to reflect
-   $("#trainTable > tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + nextTrain + "</td><td>" + minutesAway + "</td></tr>");
-})
